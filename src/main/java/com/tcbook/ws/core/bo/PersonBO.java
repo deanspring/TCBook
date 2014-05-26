7baf1f610eff253302ede88045c6ddcd5fd867d4
Caio Uvini <caio.uvini@movile.com>
2014-05-26 20:11:07 -0300

    Code formatting and artist recomendation skeleton

--- src/main/java/com/tcbook/ws/core/bo/PersonBO.java
+++ src/main/java/com/tcbook/ws/core/bo/PersonBO.java
@@ -1,5 +1,6 @@
 package com.tcbook.ws.core.bo;
 
+import com.tcbook.ws.bean.MusicalArtist;
 import com.tcbook.ws.bean.Person;
 import com.tcbook.ws.database.dao.*;
 import com.tcbook.ws.util.TCBookConstants;
@@ -14,139 +15,144 @@ import java.util.Map;
 
 public class PersonBO {
 
-	private static final Logger logEx = LoggerFactory.getLogger(TCBookConstants.LOG_NAME_EXCEPTIONS);
+    private static final Logger logEx = LoggerFactory.getLogger(TCBookConstants.LOG_NAME_EXCEPTIONS);
 
-	private PersonDAO personDAO = PersonDAOImpl.getInstance();
-	private ColleagueDAO colleagueDAO = ColleagueDAOImpl.getInstance();
-	private PersonLikeMusicalArtistDAO personLikeMusicalArtistDAO = PersonLikeMusicalArtistDAOImpl.getInstance();
+    private PersonDAO personDAO = PersonDAOImpl.getInstance();
+    private ColleagueDAO colleagueDAO = ColleagueDAOImpl.getInstance();
+    private PersonLikeMusicalArtistDAO personLikeMusicalArtistDAO = PersonLikeMusicalArtistDAOImpl.getInstance();
 
-	public List<Person> getAll() {
-		List<Person> people = personDAO.findAll();
-		return people == null ? new ArrayList<Person>() : people;
-	}
+    public List<Person> getAll() {
+        List<Person> people = personDAO.findAll();
+        return people == null ? new ArrayList<Person>() : people;
+    }
 
-	public void setColleaguesForPersonWithId(Long personId, List<Long> newColleaguesIds) {
-		removeOldColleagues(personId);
-		addNewColleagues(personId, newColleaguesIds);
-	}
+    public void setColleaguesForPersonWithId(Long personId, List<Long> newColleaguesIds) {
+        removeOldColleagues(personId);
+        addNewColleagues(personId, newColleaguesIds);
+    }
 
-	protected void addNewColleagues(Long personId, List<Long> newColleaguesIds) {
-		if (newColleaguesIds == null)
-			return;
+    protected void addNewColleagues(Long personId, List<Long> newColleaguesIds) {
+        if (newColleaguesIds == null)
+            return;
 
-		for (Long colleagueId : newColleaguesIds)
-			try {
-				colleagueDAO.insert(personId, colleagueId);
-			} catch (SQLException e) {
-				logEx.error("[TCBook] Error inserting colleague. Exception: " + e);
-				e.printStackTrace();
-			}
-	}
+        for (Long colleagueId : newColleaguesIds)
+            try {
+                colleagueDAO.insert(personId, colleagueId);
+            } catch (SQLException e) {
+                logEx.error("[TCBook] Error inserting colleague. Exception: " + e);
+                e.printStackTrace();
+            }
+    }
 
-	protected void removeOldColleagues(Long personId) {
-		List<Long> colleagueIds = colleagueDAO.findAllForId(personId);
+    protected void removeOldColleagues(Long personId) {
+        List<Long> colleagueIds = colleagueDAO.findAllForId(personId);
 
-		if (colleagueIds == null)
-			return;
+        if (colleagueIds == null)
+            return;
 
-		for (Long colleagueId : colleagueIds)
-			try {
-				colleagueDAO.remove(personId, colleagueId);
-			} catch (SQLException e) {
-				logEx.error("[TCBook] Error removing colleague. Exception: " + e);
-				e.printStackTrace();
-			}
-	}
+        for (Long colleagueId : colleagueIds)
+            try {
+                colleagueDAO.remove(personId, colleagueId);
+            } catch (SQLException e) {
+                logEx.error("[TCBook] Error removing colleague. Exception: " + e);
+                e.printStackTrace();
+            }
+    }
 
-	public List<Person> getColleaguesForPersonWithId(Long id) {
-		List<Long> colleagueIds = colleagueDAO.findAllForId(id);
-		List<Person> result = new ArrayList<Person>();
+    public List<Person> getColleaguesForPersonWithId(Long id) {
+        List<Long> colleagueIds = colleagueDAO.findAllForId(id);
+        List<Person> result = new ArrayList<Person>();
 
-		if (colleagueIds == null)
-			return result;
+        if (colleagueIds == null)
+            return result;
 
-		for (Long colleagueId : colleagueIds) {
-			Person p = personDAO.find(colleagueId);
-			if (p != null) { // should not happen, but...
-				result.add(p);
-			}
-		}
+        for (Long colleagueId : colleagueIds) {
+            Person p = personDAO.find(colleagueId);
+            if (p != null) { // should not happen, but...
+                result.add(p);
+            }
+        }
 
-		return result;
-	}
+        return result;
+    }
 
-	public Person getPerson(Long id) {
-		Person person = personDAO.find(id);
-		return person == null ? new Person() : person;
-	}
+    public Person getPerson(Long id) {
+        Person person = personDAO.find(id);
+        return person == null ? new Person() : person;
+    }
 
-	public void save(Person person) {
-		if (person == null)
-			return;
+    public void save(Person person) {
+        if (person == null)
+            return;
 
-		try {
-			if (person.getId() != null) {
-				Person oldPerson = getPerson(person.getId());
-				if (oldPerson.getId() == null)
-					return;
-				oldPerson.setHometown(person.getHometown());
-				oldPerson.setLogin(person.getLogin());
-				oldPerson.setName(person.getName());
-				oldPerson.setUrl(person.getUrl());
+        try {
+            if (person.getId() != null) {
+                Person oldPerson = getPerson(person.getId());
+                if (oldPerson.getId() == null)
+                    return;
+                oldPerson.setHometown(person.getHometown());
+                oldPerson.setLogin(person.getLogin());
+                oldPerson.setName(person.getName());
+                oldPerson.setUrl(person.getUrl());
 
-				personDAO.update(oldPerson);
-			} else {
-				person.setEnabled(true);
-				personDAO.insert(person);
-			}
-		} catch (SQLException e) {
-			logEx.error("[TCBook] Error updating person. Exception: " + e);
-		}
-	}
+                personDAO.update(oldPerson);
+            } else {
+                person.setEnabled(true);
+                personDAO.insert(person);
+            }
+        } catch (SQLException e) {
+            logEx.error("[TCBook] Error updating person. Exception: " + e);
+        }
+    }
 
-	public void disable(Long personId) {
-		Person p = personDAO.find(personId);
+    public void disable(Long personId) {
+        Person p = personDAO.find(personId);
 
-		if (p != null) {
-			p.setEnabled(Boolean.FALSE);
-			try {
-				personDAO.update(p);
-			} catch (SQLException e) {
-				logEx.error("[TCBook] Error disabling person. Exception: " + e);
-			}
-		}
-	}
+        if (p != null) {
+            p.setEnabled(Boolean.FALSE);
+            try {
+                personDAO.update(p);
+            } catch (SQLException e) {
+                logEx.error("[TCBook] Error disabling person. Exception: " + e);
+            }
+        }
+    }
 
-	public void setLikes(Long personId, Map<Long, Integer> likes) {
-		Map<Long, Integer> existingLikes = null;
+    public void setLikes(Long personId, Map<Long, Integer> likes) {
+        Map<Long, Integer> existingLikes = null;
 
-		try {
-			existingLikes = personLikeMusicalArtistDAO.getAllForPerson(personId);
-		} catch (SQLException e) {
-			logEx.error("[TCBook] Error retrieving person's like. Exception: " + e);
-		}
+        try {
+            existingLikes = personLikeMusicalArtistDAO.getAllForPerson(personId);
+        } catch (SQLException e) {
+            logEx.error("[TCBook] Error retrieving person's like. Exception: " + e);
+        }
 
-		for (Map.Entry<Long, Integer> entry : likes.entrySet()) {
-			try {
-				if (existingLikes != null && existingLikes.containsKey(entry.getKey())) {
-					personLikeMusicalArtistDAO.updateLike(personId, entry.getKey(), entry.getValue());
-				} else {
-					personLikeMusicalArtistDAO.insert(personId, entry.getKey(), entry.getValue());
-				}
-			} catch (SQLException e) {
-				logEx.error("[TCBook] Error registering person's like. Exception: " + e);
-			}
-		}
-	}
+        for (Map.Entry<Long, Integer> entry : likes.entrySet()) {
+            try {
+                if (existingLikes != null && existingLikes.containsKey(entry.getKey())) {
+                    personLikeMusicalArtistDAO.updateLike(personId, entry.getKey(), entry.getValue());
+                } else {
+                    personLikeMusicalArtistDAO.insert(personId, entry.getKey(), entry.getValue());
+                }
+            } catch (SQLException e) {
+                logEx.error("[TCBook] Error registering person's like. Exception: " + e);
+            }
+        }
+    }
 
-	public Map<Long, Integer> getMusicalsArtistsForPersonWithId(Long personId) {
-		try {
-			return personLikeMusicalArtistDAO.getAllForPerson(personId);
-		} catch (Exception e) {
-			logEx.error("[TCBook] Error retrieving musicalArtists that person {} like. Exception: " + e, personId);
+    public Map<Long, Integer> getMusicalsArtistsForPersonWithId(Long personId) {
+        try {
+            return personLikeMusicalArtistDAO.getAllForPerson(personId);
+        } catch (Exception e) {
+            logEx.error("[TCBook] Error retrieving musicalArtists that person {} like. Exception: " + e, personId);
 
-			return new HashMap<Long, Integer>();
-		}
-	}
+            return new HashMap<Long, Integer>();
+        }
+    }
+
+    public List<MusicalArtist> recommendArtistsForPerson(Long personId) {
+        //TODO
+        return null;
+    }
 
 }
--- src/main/java/com/tcbook/ws/web/rest/PeopleRS.java
+++ src/main/java/com/tcbook/ws/web/rest/PeopleRS.java
@@ -2,9 +2,11 @@ package com.tcbook.ws.web.rest;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.tcbook.ws.bean.MusicalArtist;
 import com.tcbook.ws.bean.Person;
 import com.tcbook.ws.core.bo.PersonBO;
 import com.tcbook.ws.util.TCBookConstants;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,89 +18,113 @@ import java.util.Map;
 @Path("/people")
 public class PeopleRS {
 
-	private static Logger logReqAnswered = LoggerFactory.getLogger(TCBookConstants.LOG_NAME_REQUESTS_ANSWERED);
+    private static Logger logReqAnswered = LoggerFactory.getLogger(TCBookConstants.LOG_NAME_REQUESTS_ANSWERED);
+    private static Logger logEx = LoggerFactory.getLogger(TCBookConstants.LOG_NAME_EXCEPTIONS);
 
-	private static PersonBO personBO = new PersonBO();
+    private static PersonBO personBO = new PersonBO();
 
-	@POST
-	@Path("/")
-	public Response newPerson(@FormParam("person") String personJson) {
-		logReqAnswered.info("save new person.");
-		personBO.save(new Gson().fromJson(personJson, Person.class));
+    @POST
+    @Path("/")
+    public Response newPerson(@FormParam("person") String personJson) {
+        logReqAnswered.info("save new person.");
+        personBO.save(new Gson().fromJson(personJson, Person.class));
 
-		return Response.ok().build();
-	}
+        return Response.ok().build();
+    }
 
-	@PUT
-	@Path("/{id}")
-	public Response editPerson(@PathParam("id") Long id, @FormParam("person") String personJson) {
-		logReqAnswered.info("edit person " + id + ".");
-		personBO.save(new Gson().fromJson(personJson, Person.class));
+    @PUT
+    @Path("/{id}")
+    public Response editPerson(@PathParam("id") Long id, @FormParam("person") String personJson) {
+        logReqAnswered.info("edit person " + id + ".");
+        personBO.save(new Gson().fromJson(personJson, Person.class));
 
-		return Response.ok().build();
-	}
+        return Response.ok().build();
+    }
 
-	@GET
-	@Path("/")
-	public Response getAll() {
-		logReqAnswered.info("list of people.");
+    @GET
+    @Path("/")
+    public Response getAll() {
+        logReqAnswered.info("list of people.");
 
-		return Response.ok(new Gson().toJson(personBO.getAll())).build();
-	}
+        return Response.ok(new Gson().toJson(personBO.getAll())).build();
+    }
 
-	@GET
-	@Path("/{id}")
-	public Response getById(@PathParam("id") Long id) {
-		logReqAnswered.info("get person " + id);
+    @GET
+    @Path("/{id}")
+    public Response getById(@PathParam("id") Long id) {
+        logReqAnswered.info("get person " + id);
 
-		return Response.ok(new Gson().toJson(personBO.getPerson(id))).build();
-	}
+        return Response.ok(new Gson().toJson(personBO.getPerson(id))).build();
+    }
 
-	@DELETE
-	@Path("/{id}")
-	public Response removeById(@PathParam("id") Long id) {
-		logReqAnswered.info("delete person " + id);
+    @DELETE
+    @Path("/{id}")
+    public Response removeById(@PathParam("id") Long id) {
+        logReqAnswered.info("delete person " + id);
 
-		personBO.disable(id);
+        personBO.disable(id);
 
-		return Response.ok().build();
-	}
+        return Response.ok().build();
+    }
 
-	@GET
-	@Path("/{id}/social")
-	public Response getSocialById(@PathParam("id") Long id) {
-		logReqAnswered.info("get knows of the person " + id);
+    @GET
+    @Path("/{id}/social")
+    public Response getSocialById(@PathParam("id") Long id) {
+        logReqAnswered.info("get knows of the person " + id);
 
-		return Response.ok(new Gson().toJson(personBO.getColleaguesForPersonWithId(id))).build();
-	}
+        return Response.ok(new Gson().toJson(personBO.getColleaguesForPersonWithId(id))).build();
+    }
 
-	@PUT
-	@Path("/{id}/social")
-	public Response setSocialById(@PathParam("id") Long id, @FormParam("social") String colleaguesIds) {
-		logReqAnswered.info("set knows of the person " + id);
+    @PUT
+    @Path("/{id}/social")
+    public Response setSocialById(@PathParam("id") Long id, @FormParam("social") String colleaguesIds) {
+        logReqAnswered.info("set knows of the person " + id);
 
-		personBO.setColleaguesForPersonWithId(id, new Gson().<List<Long>> fromJson(colleaguesIds, new TypeToken<List<Long>>() {
-		}.getType()));
+        personBO.setColleaguesForPersonWithId(id, new Gson().<List<Long>>fromJson(colleaguesIds, new TypeToken<List<Long>>() {
+        }.getType()));
 
-		return Response.ok().build();
-	}
+        return Response.ok().build();
+    }
 
-	@GET
-	@Path("/{id}/musicalArtists")
-	public Response getMusicalArtistById(@PathParam("id") Long id) {
-		logReqAnswered.info("get musicals artists of the person " + id);
+    @GET
+    @Path("/{id}/musicalArtists")
+    public Response getMusicalArtistById(@PathParam("id") Long id) {
+        logReqAnswered.info("get musicals artists of the person " + id);
 
-		return Response.ok(new Gson().toJson(personBO.getMusicalsArtistsForPersonWithId(id))).build();
-	}
+        return Response.ok(new Gson().toJson(personBO.getMusicalsArtistsForPersonWithId(id))).build();
+    }
 
-	@PUT
-	@Path("/{id}/musicalArtist")
-	public Response setMusicalArtistById(@PathParam("id") Long id, @FormParam("artists") String artistsIds) {
-		logReqAnswered.info("set musicals Artists of the person " + id);
+    @PUT
+    @Path("/{id}/musicalArtist")
+    public Response setMusicalArtistById(@PathParam("id") Long id, @FormParam("artists") String artistsIds) {
+        logReqAnswered.info("set musicals Artists of the person " + id);
 
-		personBO.setLikes(id, new Gson().<Map<Long, Integer>> fromJson(artistsIds, new TypeToken<Map<Long, Integer>>() {
-		}.getType()));
+        personBO.setLikes(id, new Gson().<Map<Long, Integer>>fromJson(artistsIds, new TypeToken<Map<Long, Integer>>() {
+        }.getType()));
 
-		return Response.ok().build();
-	}
+        return Response.ok().build();
+    }
+
+    @GET
+    @Path("/{id}/recommend")
+    public Response recommendArtistsByPersonId(@PathParam("id") Long personId) {
+        long before = System.currentTimeMillis();
+
+        List<MusicalArtist> recommendedArtists = personBO.recommendArtistsForPerson(personId);
+
+        String artistsJson = "";
+
+        try {
+            if (recommendedArtists != null) {
+                artistsJson = new ObjectMapper().writeValueAsString(recommendedArtists);
+            }
+        } catch (Exception e) {
+            logEx.error("Error converting recommended artists {} to json", recommendedArtists, e);
+        }
+
+        logReqAnswered.info("recommended artists for person {}: {}. Time it took " + (System.currentTimeMillis() - before) + "ms", personId, recommendedArtists);
+
+        return Response.ok(artistsJson).build();
+
+    }
 }
